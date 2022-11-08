110e6bd39978ff0d839a38d5358059d14f053eda
eettlin <mrumrocks@gmail.com>
2022-11-08 08:41:29 -0800

    221108 Added food particle system

---
+++ testbranch/foodps1.js
@@ -0,0 +1,32 @@
+// A system of particles
+
+class FoodPS1 {
+    constructor(loc, ctx) {
+        this.loc = loc;
+        this.ctx = ctx;
+        this.count = 333;
+        this.particles = [];    // list of particles
+    }
+
+    addParticle() {
+        // pick a random particle generator for this particle
+        this.particles.push(new Particle(this.loc, this.ctx));
+    }
+
+    run() {
+        for (let i = this.particles.length - 1; i >= 0; i--) {
+            this.particles[i].run();
+            if (this.particles[i].isDead) {
+                this.particles.splice(i, 1); // remove this particle
+            }
+        }
+        if(++this.count%111 === 0 &&  this.particles.length < 15){
+            this.addParticle(this.loc, this.ctx); // new particle each time
+            this.count = 0;
+        }
+        
+    }
+}
+
+
+
--- world.js
+++ world.js
@@ -45,18 +45,20 @@ class World {
   //Load mover array
   loadEntities(numEntities, ctx1, w, h) {
 
+    let location = new JSVector(300, 300);
+    this.entities.push(new FoodPS1(location, this.ctxMain));
     //  generic entities
-    for (let i = 0; i < numEntities; i++) {
-      let diam = 3;
-      let x = Math.random() * (this.dims.width - 2 * diam) + diam - this.dims.width / 2;
-      let y = Math.random() * (this.dims.height - 2 * diam) + diam - this.dims.height / 2;
-      let loc = new JSVector(x, y);
-      let dx = Math.random() * 2 - 1;
-      let dy = Math.random() * 2 - 1;
-      let vel = new JSVector(dx, dy);
-      this.entities.push(new Entity(loc, vel, diam, this));
-    }
-    //  generic creatures
+  //   for (let i = 0; i < numEntities; i++) {
+  //     let diam = 3;
+  //     let x = Math.random() * (this.dims.width - 2 * diam) + diam - this.dims.width / 2;
+  //     let y = Math.random() * (this.dims.height - 2 * diam) + diam - this.dims.height / 2;
+  //     let loc = new JSVector(x, y);
+  //     let dx = Math.random() * 2 - 1;
+  //     let dy = Math.random() * 2 - 1;
+  //     let vel = new JSVector(dx, dy);
+  //     this.entities.push(new Entity(loc, vel, diam, this));
+  //   }
+  //   //  generic creatures
     for (let i = 0; i < 500; i++) {
       let x = Math.random() * this.dims.width - this.dims.width / 2;
       let y = Math.random() * this.dims.height - this.dims.height / 2;
@@ -68,18 +70,18 @@ class World {
           this)
       );
     }
-    //  generic food
-    for (let i = 0; i < 500; i++) {
-      let x = Math.random() * (this.dims.width-20) - (this.dims.width / 2 - 10);
-      let y = Math.random() * (this.dims.height-20) - (this.dims.height / 2 - 10);
-      let loc = new JSVector(x, y);
-      this.entities.push(
-          new Food(loc,
-          new JSVector(0, 0),
-          6,
-          this)
-      );
-    }
+  //   //  generic food
+  //   for (let i = 0; i < 500; i++) {
+  //     let x = Math.random() * (this.dims.width-20) - (this.dims.width / 2 - 10);
+  //     let y = Math.random() * (this.dims.height-20) - (this.dims.height / 2 - 10);
+  //     let loc = new JSVector(x, y);
+  //     this.entities.push(
+  //         new Food(loc,
+  //         new JSVector(0, 0),
+  //         6,
+  //         this)
+  //     );
+  //   }
   }//++++++++++++++++++++++++++++  load entities
 
 
