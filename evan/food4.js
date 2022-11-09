28784c341d33773de00535493b5b7d8b55635479
807116 <807116@seq.org>
2022-11-09 12:19:10 -0800

    Update food4.js

--- evan/food4.js
+++ evan/food4.js
@@ -1,7 +1,13 @@
 class Food4 extends Entity {
     // properties
     constructor(loc, vel, sz, wrld) {
-        super(loc, vel, sz, wrld)
+        super(loc, vel, sz, wrld);
+        this.loc = loc;
+        this.vel = vel;
+        this.world = wrld;
+        this.size = sz;
+        this.alive = true;sdwqqqqqqqqqq
+        this.clr = clr;
         
     
     }
@@ -9,24 +15,38 @@ class Food4 extends Entity {
     run() {
         this.update();
         this.render();
+        this.lifespan();
     }
 
     update() {
     }
 
     render() {
-       
+        this.ctx.beginPath();
+        this.ctx.arc(this.loc.x, this.loc.y, this.rad, 0, Math.PI * 2);
+        this.ctx.closePath();
+        this.ctx.strokeStyle = this.clr;
+        this.ctx.fillStyle = this.clr;
+        this.ctx.fill();
+        this.ctx.stroke();    
+    }
+
+    lifespan(){
+        this.life = health;
+        if (this.life <= 0) {
+            this.alive = false;
+        }
     }
 
     getRandomColor() {
         //  List of hex color values for movers
         let colors = [
-            "#25AA34",
-            "#18CC2e",
-            "#389925",
-            "#11AA99",
-            "#99CC00",
-            "#11FF65"
+            rgba(37,170,52,1),
+            rgba(24,204,46,1),
+            rgba(56,153,37,1),
+            rgba(17,170,153,1),
+            rgba(153,204,0,1),
+            rgba(17,255,101,1)
         ];
         let index = Math.floor(Math.random() * colors.length);
         return colors[index];
